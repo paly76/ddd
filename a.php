acebeebb2a8a332f56318cab68cdb061527af87e
panzhaodong@appgame.com <panzhaodong@appgame.com>
2017-09-29 14:00:10 +0800

    add file data.php; modify a.php

--- a.php
+++ a.php
@@ -1,3 +1,30 @@
-dsadsa
-dsadsadsa
-999999
+<?php
+function getDirFile($dir, &$arr = []) {
+	if(!is_dir($dir)) return false ;
+	$fp = opendir($dir);
+	while(false!==($fileName = readdir($fp))) {
+		if($fileName != '.' && $fileName != '..') {
+			$subFile = $dir. '/' .$fileName ;
+			if(is_file($subFile)) $arr[] = $subFile ;
+			if(is_dir($subFile)) getDirFile($subFile);
+		}
+	}
+	closedir($fp);
+	return $arr ;
+}
+
+function getArrSort(array $arr) {
+	if(!is_array($arr)) return false ;
+	$len = count($arr);
+	for($i=0; $i< $len; $i++) {
+		for($j=$len-i; $j>=i; $j--) {
+			if($i<=$i-1) {
+				$temp = $arr[$i] ;
+				$arr[$i] = $arr[$i-1];
+			       	$arr[$i-1] = $temp;	
+			} 
+		}
+	}
+	return $arr;
+}
+
---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+return [
+	'user' => 'root',
+	'password'=>'111111',
+	'localhost'=>'127.0.0.1',
+	'db_data' => 'users',
+	'mysql_charset' => 'utf8'
+];
